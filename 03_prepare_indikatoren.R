4a318c1a2144e4e582819b37decbcd0eb173a741
FLorenz <felixlorenz18@gmail.com>
2025-03-13 13:05:12 +0000

    remove attempt dep

--- 03_prepare_indikatoren.R
+++ 03_prepare_indikatoren.R
@@ -157,10 +157,8 @@ get_ogd_catalog <- function (){
 
 
 get_data_from_ogd <- function (dataset_id){
-  attempt::stop_if_not(dataset_id, is.character, msg = "dataset_id has to be a character string")
   res = httr::GET(glue::glue("https://data.tg.ch/api/v2/catalog/datasets/{dataset_id}/exports/json"))
-  attempt::stop_if_not(.x = httr::status_code(res), .p = ~.x ==
-                         200, msg = "The API returned an error. Please check your internet connection or visit data.tg.ch for more information")
+
   result = jsonlite::fromJSON(rawToChar(res$content), flatten = TRUE)
   return(result)
 }
