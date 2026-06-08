87266f1d29ce4a3f737eeefbf3bcfd88f9d35ed7
FLorenz <felixlorenz18@gmail.com>
2026-06-08 15:32:07 +0200

    delete

--- base_db/R/app_server.R
+++
@@ -1,8 +0,0 @@
-#' Die Shiny-Applikation Server-Logik
-#'
-#' @param input,output,session Interne Shiny-Parameter
-#' @noRd
-app_server <- function(input, output, session) {
-  # Server-Logik hier hinzufuegen
-}
-
--- indikatoren_db/R/app_server.R
+++
@@ -1,9 +0,0 @@
-#' Die Shiny-Applikation Server-Logik
-#'
-#' @param input,output,session Interne Shiny-Parameter
-#' @noRd
-app_server <- function(input, output, session) {
-  # Module einbinden
-  mod_karte_server("karte")
-}
-
